9706ef567d8acaccad012227152e2dfa2b9a65d8
AshWilliamns <robert.rozas.n@gmail.com>
2017-12-19 13:11:39 -0300

    Se añade clase documentdb

---
+++ Models/CosmosDB.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+
+
+namespace dotnetcoremvc.Models
+{
+    public static class CosmosDB
+    {
+        private static readonly string endPointUrl = "";
+        private static readonly string authKey = "sadasdgasd";
+        public static void test(){
+            
+        }   
+    }
+}
