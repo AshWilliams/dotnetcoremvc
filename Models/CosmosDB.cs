bc86a34b4aab0a8f39111d62975ea204443f7c9e
AshWilliamns <robert.rozas.n@gmail.com>
2017-12-21 10:15:31 -0300

    Se añaden métodos inicialización, creación y consultas

--- Models/CosmosDB.cs
+++ Models/CosmosDB.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
+using System.Net;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace dotnetcoremvc.Models
@@ -12,21 +14,39 @@ namespace dotnetcoremvc.Models
         private static readonly string authKey = "OKKSsPamNJAlY3i5Fjm2juEKXl6Byb9RHxX4pIZS774qtQWmlrXTjyApmygctd2m4J5eZsLDm5LsoIXH2xZqcg==";
         private static DocumentClient cosmosClient = new DocumentClient(new Uri(endPointUrl),authKey);
 
-        private static string databaseName = "MyCosmosDB";
-        private static string collectionName = "MyCosmosCollection";
+        private static string databaseName = "MyCosmosTodoDB";
+        private static string collectionName = "MyCosmosToDoCollection";
         public static async Task Initialize(){
             await cosmosClient.CreateDatabaseIfNotExistsAsync(new Database{Id = databaseName});
             await cosmosClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseName), new DocumentCollection{Id = collectionName});
         }
 
-        public static async Task<int> CreateToDoTaskAsync(){
+        public static async Task<int> CreateToDoTaskAsync(ToDoTasks theTask){
             try{
-
+                await cosmosClient.ReadDocumentAsync(UriFactory.CreateDocumentUri(databaseName,collectionName,theTask.id));
+                //Found
+                return 0;
             }
             catch(DocumentClientException de){
-
+                if(de.StatusCode == HttpStatusCode.NotFound){
+                    await cosmosClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseName,collectionName),theTask);
+                }
+                else{
+                    throw;
+                }
+                return 1;
             }
         } 
+        public static IQueryable<ToDoTasks> SelectDocuments(){
+            FeedOptions queryOptions = new FeedOptions{MaxItemCount = -1};
+            //Linq way
+             IQueryable<ToDoTasks> toDoQuery = cosmosClient.CreateDocumentQuery<ToDoTasks>(
+                UriFactory.CreateDocumentCollectionUri(databaseName,collectionName),
+                "SELECT * FROM MyCosmosToDoCollection",
+                queryOptions);
+
+            return toDoQuery;
+        }
 
     }
 }
